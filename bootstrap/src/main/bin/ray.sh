7142b85d8439c0f11be4df2c498101bbd2e04022
Gregory Amerson <gregory.amerson@liferay.com>
2013-09-04 13:07:23 +0800

    fixed shell scripts

--- bootstrap/src/main/bin/ray.sh
+++ bootstrap/src/main/bin/ray.sh
@@ -11,12 +11,12 @@ while [ -h "$PRG" ]; do
         PRG=`dirname "$PRG"`/"$link"
     fi
 done
-ray_HOME=`dirname "$PRG"`
+RAY_HOME=`dirname "$PRG"`
 
 # Absolute path
-ray_HOME=`cd "$ray_HOME/.." ; pwd`
+RAY_HOME=`cd "$RAY_HOME/.." ; pwd`
 
-# echo Resolved ray_HOME: $ray_HOME
+# echo Resolved RAY_HOME: $RAY_HOME
 # echo "JAVA_HOME $JAVA_HOME"
 
 cygwin=false;
@@ -27,19 +27,19 @@ case "`uname`" in
 esac
 
 # Build a classpath containing our two magical startup JARs (we look for " /" as per ray-905)
-ray_CP=`echo "$ray_HOME"/bin/*.jar | sed 's/ \//:\//g'`
-# echo ray_CP: $ray_CP
+RAY_CP=`echo "$RAY_HOME"/bin/*.jar | sed 's/ \//:\//g'`
+# echo RAY_CP: $RAY_CP
 
 # Store file locations in variables to facilitate Cygwin conversion if needed
 
-ray_OSGI_FRAMEWORK_STORAGE="$ray_HOME/cache"
-# echo "ray_OSGI_FRAMEWORK_STORAGE: $ray_OSGI_FRAMEWORK_STORAGE"
+RAY_OSGI_FRAMEWORK_STORAGE="$RAY_HOME/cache"
+# echo "RAY_OSGI_FRAMEWORK_STORAGE: $RAY_OSGI_FRAMEWORK_STORAGE"
 
-ray_AUTO_DEPLOY_DIRECTORY="$ray_HOME/bundle"
-# echo "ray_AUTO_DEPLOY_DIRECTORY: $ray_AUTO_DEPLOY_DIRECTORY"
+RAY_AUTO_DEPLOY_DIRECTORY="$RAY_HOME/bundle"
+# echo "RAY_AUTO_DEPLOY_DIRECTORY: $RAY_AUTO_DEPLOY_DIRECTORY"
 
-ray_CONFIG_FILE_PROPERTIES="$ray_HOME/conf/config.properties"
-# echo "ray_CONFIG_FILE_PROPERTIES: $ray_CONFIG_FILE_PROPERTIES"
+RAY_CONFIG_FILE_PROPERTIES="$RAY_HOME/conf/config.properties"
+# echo "RAY_CONFIG_FILE_PROPERTIES: $RAY_CONFIG_FILE_PROPERTIES"
 
 cygwin=false;
 case "`uname`" in
@@ -49,16 +49,16 @@ case "`uname`" in
 esac
 
 if [ "$cygwin" = "true" ]; then
-    export ray_HOME=`cygpath -wp "$ray_HOME"`
-    export ray_CP=`cygpath -wp "$ray_CP"`
-    export ray_OSGI_FRAMEWORK_STORAGE=`cygpath -wp "$ray_OSGI_FRAMEWORK_STORAGE"`
-    export ray_AUTO_DEPLOY_DIRECTORY=`cygpath -wp "$ray_AUTO_DEPLOY_DIRECTORY"`
-    export ray_CONFIG_FILE_PROPERTIES=`cygpath -wp "$ray_CONFIG_FILE_PROPERTIES"`
-    # echo "Modified ray_HOME: $ray_HOME"
-    # echo "Modified ray_CP: $ray_CP"
-    # echo "Modified ray_OSGI_FRAMEWORK_STORAGE: $ray_OSGI_FRAMEWORK_STORAGE"
-    # echo "Modified ray_AUTO_DEPLOY_DIRECTORY: $ray_AUTO_DEPLOY_DIRECTORY"
-    # echo "Modified ray_CONFIG_FILE_PROPERTIES: $ray_CONFIG_FILE_PROPERTIES"
+    export RAY_HOME=`cygpath -wp "$RAY_HOME"`
+    export RAY_CP=`cygpath -wp "$RAY_CP"`
+    export RAY_OSGI_FRAMEWORK_STORAGE=`cygpath -wp "$RAY_OSGI_FRAMEWORK_STORAGE"`
+    export RAY_AUTO_DEPLOY_DIRECTORY=`cygpath -wp "$RAY_AUTO_DEPLOY_DIRECTORY"`
+    export RAY_CONFIG_FILE_PROPERTIES=`cygpath -wp "$RAY_CONFIG_FILE_PROPERTIES"`
+    # echo "Modified RAY_HOME: $RAY_HOME"
+    # echo "Modified RAY_CP: $RAY_CP"
+    # echo "Modified RAY_OSGI_FRAMEWORK_STORAGE: $RAY_OSGI_FRAMEWORK_STORAGE"
+    # echo "Modified RAY_AUTO_DEPLOY_DIRECTORY: $RAY_AUTO_DEPLOY_DIRECTORY"
+    # echo "Modified RAY_CONFIG_FILE_PROPERTIES: $RAY_CONFIG_FILE_PROPERTIES"
 fi
 
 # make sure to disable the flash message feature for the default OSX terminal, we recommend to use a ANSI compliant terminal such as iTerm if flash message support is desired
@@ -69,6 +69,6 @@ fi
 
 ANSI="-Dray.console.ansi=true"
 # Hop, hop, hop...
-java -Dis.apple.terminal=$APPLE_TERMINAL $ray_OPTS $ANSI -Dray.args="$*" -DdevelopmentMode=false -Dorg.osgi.framework.storage="$ray_OSGI_FRAMEWORK_STORAGE" -Dfelix.auto.deploy.dir="$ray_AUTO_DEPLOY_DIRECTORY" -Dfelix.config.properties="file:$ray_CONFIG_FILE_PROPERTIES" -cp "$ray_CP" com.liferay.cli.bootstrap.Main
+java -Dis.apple.terminal=$APPLE_TERMINAL $RAY_OPTS $ANSI -Dray.args="$*" -DdevelopmentMode=false -Dorg.osgi.framework.storage="$RAY_OSGI_FRAMEWORK_STORAGE" -Dfelix.auto.deploy.dir="$RAY_AUTO_DEPLOY_DIRECTORY" -Dfelix.config.properties="file:$RAY_CONFIG_FILE_PROPERTIES" -cp "$RAY_CP" com.liferay.cli.bootstrap.Main
 EXITED=$?
 # echo ray exited with code $EXITED
